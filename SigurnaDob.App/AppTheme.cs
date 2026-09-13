828fdfa2623f7fbdae118496527afd80c9c94d52
dsevar1 <dsevar00@gmail.com>
2026-09-13 17:05:51 +0200

    Promjena glavne boje aplikacije - Material Blue umjesto ljubičaste

---
+++ SigurnaDob.App/AppTheme.cs
@@ -0,0 +1,20 @@
+using MudBlazor;
+
+namespace SigurnaDob.App;
+
+public static class AppTheme
+{
+    public static readonly MudTheme Theme = new()
+    {
+        PaletteLight = new PaletteLight
+        {
+            Primary = "#1976D2",
+            AppbarBackground = "#1976D2"
+        },
+        PaletteDark = new PaletteDark
+        {
+            Primary = "#1976D2",
+            AppbarBackground = "#1976D2"
+        }
+    };
+}
